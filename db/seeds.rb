4f6170d4ba34a0798acbfb561bfd5bbf9006e3f4
YY Ong <yuanyng@outlook.com>
2022-08-01 22:37:48 +0800

    seeded movies db with API

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+puts "Creating movies..."
+
+movies["results"].each do |result|
+  movie = Movie.new(
+    title: result["title"],
+    overview: result["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{result["poster_path"]}",
+    rating: result["vote_average"]
+  )
+  movie.save
+  puts "Created Movie #{movie.id} for #{movie.title}"
+end
+
+puts "Finished!"
