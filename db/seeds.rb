f2d721a681e04c1d8918bb83b80738e3225ba7bb
YY Ong <yuanyng@outlook.com>
2022-08-03 20:33:33 +0800

    implemented list mvc

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,17 @@
 require "json"
 require "open-uri"
 
+# scrape movies fr web
 url = "https://tmdb.lewagon.com/movie/top_rated"
 movies_serialized = URI.open(url).read
 movies = JSON.parse(movies_serialized)
 
-puts "Cleaning database..."
+# first destroy all bookmarks
+puts "Cleaning bookmarks database..."
+Bookmark.destroy_all
+
+# destroy all movies and then seed movies
+puts "Cleaning movies database..."
 Movie.destroy_all
 
 puts "Creating movies..."
@@ -29,4 +35,27 @@ movies["results"].each do |result|
   puts "Created Movie #{movie.id} for #{movie.title}"
 end
 
-puts "Finished!"
+puts "Finished creating movies!"
+
+# destroy all lists and then seed lists
+puts "Cleaning lists database..."
+List.destroy_all
+
+List.create(name: "History", image_url: "https://images.unsplash.com/photo-1515325595179-59cd5262ca53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80")
+List.create(name: "Mystery", image_url: "https://images.unsplash.com/photo-1523804427826-322aa3cfaa42?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")
+List.create(name: "Non-English", image_url: "https://images.unsplash.com/photo-1505782045004-7a51dc8e0ece?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=897&q=80")
+
+puts "Finished creating lists!"
+
+#seed bookmarks
+bookmark = Bookmark.new(comment: "Winner of seven Academy Awards, including Best Picture and Best Director, this incredible true story follows the enigmatic Oskar Schindler")
+bookmark.list = List.find_by(name: "History")
+bookmark.movie = Movie.find_by(title: "Schindler's List")
+bookmark.save
+
+bookmark = Bookmark.new(comment: "Based on Stephen King's novel")
+bookmark.list = List.find_by(name: "Mystery")
+bookmark.movie = Movie.find_by(title: "The Green Mile")
+bookmark.save
+
+puts "Finished creating bookmarks!"
